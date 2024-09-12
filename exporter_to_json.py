93b3d16e662641b964a7d29c730207c77afc9256
Rodolfo Bortolin <rodolfobortolin@gmail.com>
2024-09-12 15:41:14 -0300

    Update and rename cloud_exporter_to_json.py to exporter_to_json.py

--- exporter_to_json.py
+++ exporter_to_json.py
@@ -9,21 +9,60 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
-# Global variables and constants
 MAX_FILE_SIZE_MB = 7
 MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
+
+def select_jira_version():
+    """
+    Displays a checklist for the user to select the Jira version.
+    """
+    print("Please select the Jira version you are using:")
+    print("1. Jira Cloud")
+    print("2. Jira Data Center")
+
+    while True:
+        choice = input("Enter the corresponding number (1 or 2): ").strip()
+
+        if choice == '1':
+            return 'cloud'
+        elif choice == '2':
+            return 'datacenter'
+        else:
+            print("Invalid input. Please choose 1 for Jira Cloud or 2 for Jira Data Center.")
+
+
+JIRA_VERSION = select_jira_version()
 PROJECT_KEY = input("Enter the project key you want to export: ")
 
-config = {
+if JIRA_VERSION == 'cloud':
+    config = {
+        'email': 'rodolfobortolin@gmail.com',
+        'token': '',
+        'base_url': "https://bortolin.atlassian.net",
+        'auth_type': 'token'
+    }
+    
+elif JIRA_VERSION == 'datacenter':
+    config = {
+        'username': 'admin', 
+        'password': 'admin',  
+        'base_url': "http://localhost:8080",
+        'auth_type': 'basic'
+    }
+
+CUSTOM_USER = "712020:e5165038-2f2b-4650-a575-e61739ca7376"
+
+cloud_config = {
     'email': 'rodolfobortolin@gmail.com',
     'token': '',
     'base_url': "https://bortolin.atlassian.net",
 }
 
-CUSTOM_COMMENT_AUTHOR = "712020:e5165038-2f2b-4650-a575-e61739ca7376"
 USER_CACHE_FILE = "users_cache.txt"
+USER_ACCOUNTS = "users_accounts.txt"
 PROCESSED_ISSUES_CACHE = "processed_issues_cache.txt"
-EXEMPTED_GROUPS = ["org-admins"]
+EXEMPTED_GROUPS = ["849753"]
+
 
 ALLOWED_CUSTOM_FIELD_TYPES = [
     "com.atlassian.jira.plugin.system.customfieldtypes:textfield",
@@ -55,6 +94,31 @@ issue_id_map = {}
 current_issue_id = 1
 link_id_counter = 1
 
+def get_auth():
+    if config['auth_type'] == 'token':
+        return HTTPBasicAuth(config['email'], config['token'])
+    else:
+        return HTTPBasicAuth(config['username'], config['password'])
+
+def fetch_account_id_by_email(email):
+    """
+    Fetch the accountId of a user based on their email by querying the Jira Cloud API.
+    Always uses the credentials specific to Jira Cloud.
+    """
+    url = f"{cloud_config['base_url']}/rest/api/3/user/search?query={email}"
+    response = requests.get(url, auth=HTTPBasicAuth(cloud_config['email'], cloud_config['token']),
+                            headers={"Accept": "application/json"})
+
+    if response.status_code != 200:
+        logging.error(f"Error fetching accountId for email {email}: {response.status_code}")
+        return CUSTOM_USER  
+
+    users = response.json()
+    if users and isinstance(users, list):
+        return users[0].get('accountId', CUSTOM_USER) 
+
+    return CUSTOM_USER 
+
 def get_next_issue_id():
     global current_issue_id
     issue_id = current_issue_id
@@ -68,40 +132,113 @@ def get_next_link_id():
     return link_id
 
 def load_user_cache():
+    """
+    Load the user cache from USER_CACHE_FILE (groups, permissions, etc.)
+    """
     user_cache = {}
     if os.path.exists(USER_CACHE_FILE):
         with file_lock:
             with open(USER_CACHE_FILE, 'r') as file:
                 for line in file:
                     try:
-                        user_key, is_in_group = line.strip().split(',', 1)
-                        user_cache[user_key] = is_in_group
+                        email, in_group = line.strip().split(',', 1)
+                        user_cache[email] = in_group
                     except ValueError:
                         continue
     return user_cache
 
+def load_user_accounts():
+    """
+    Load the user account mappings from USER_ACCOUNTS (email to accountId).
+    """
+    user_accounts = {}
+    if os.path.exists(USER_ACCOUNTS):
+        with file_lock:
+            with open(USER_ACCOUNTS, 'r') as file:
+                for line in file:
+                    try:
+                        email, account_id = line.strip().split(',', 1)
+                        user_accounts[email] = account_id
+                    except ValueError:
+                        continue
+    return user_accounts
+
 def save_user_cache(user_cache):
+    """
+    Save the user cache to USER_CACHE_FILE (groups, permissions, etc.)
+    """
     with file_lock:
         with open(USER_CACHE_FILE, 'w') as file:
-            for user_key, is_in_group in user_cache.items():
-                file.write(f"{user_key},{is_in_group}\n")
+            for email, in_group in user_cache.items():
+                file.write(f"{email},{in_group}\n")
+
+def save_user_accounts(user_accounts):
+    """
+    Save the user account mappings to USER_ACCOUNTS (email to accountId).
+    """
+    existing_accounts = load_user_accounts()
+    existing_accounts.update(user_accounts)  # Atualiza os dados existentes com os novos
+
+    with file_lock:
+        with open(USER_ACCOUNTS, 'w') as file:
+            for email, account_id in existing_accounts.items():
+                file.write(f"{email},{account_id}\n")
 
 def cache_user_group(user_key, in_exempted_groups, user_cache):
     if user_key not in user_cache:
         user_cache[user_key] = str(in_exempted_groups)
         save_user_cache(user_cache)
 
+def fetch_user_group_membership_cloud(user_key):
+    
+    url = f"{cloud_config['base_url']}/rest/api/2/user?accountId={user_key}&expand=groups"
+    response = requests.get(url, auth=HTTPBasicAuth(cloud_config['email'], cloud_config['token']),
+                            headers={"Accept": "application/json"})
+    if response.status_code != 200:
+        logging.error(f"Error fetching groups for user {user_key} in Jira Cloud: {response.status_code}")
+        return False
+    user_data = response.json()
+    groups = [group['name'] for group in user_data['groups']['items']]
+    
+    return any(group in EXEMPTED_GROUPS for group in groups)
+
+def fetch_user_group_membership_datacenter(user_key):
+    
+    url = f"{config['base_url']}/rest/api/2/user?username={user_key}&expand=groups"
+    response = requests.get(url, auth=HTTPBasicAuth(config['username'], config['password']),
+                            headers={"Accept": "application/json"})
+    if response.status_code != 200:
+        logging.error(f"Error fetching groups for user {user_key} in Jira Data Center: {response.status_code}")
+        return False
+    user_data = response.json()
+    groups = [group['name'] for group in user_data['groups']['items']]
+    
+    return any(group in EXEMPTED_GROUPS for group in groups)
+
 def is_user_in_exempted_groups(user_key, user_cache):
+    """
+    Check if the user is in an exempted group.
+    In Jira Data Center, check groups from Data Center, and in Jira Cloud, check groups from Cloud.
+    """
     if user_key in user_cache:
         return user_cache[user_key] == 'True'
-    in_exempted_groups = fetch_user_group_membership(user_key)
+
+    if JIRA_VERSION == 'cloud':
+        in_exempted_groups = fetch_user_group_membership_cloud(user_key)
+    else:
+        in_exempted_groups = fetch_user_group_membership_datacenter(user_key)
+
     cache_user_group(user_key, in_exempted_groups, user_cache)
+
     return in_exempted_groups
 
 def fetch_user_group_membership(user_key):
-    url = f"{config['base_url']}/rest/api/2/user?accountId={user_key}&expand=groups"
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
-                            headers={"Accept": "application/json"})
+    if JIRA_VERSION == 'cloud':
+        url = f"{config['base_url']}/rest/api/2/user?accountId={user_key}&expand=groups"
+    else:
+        url = f"{config['base_url']}/rest/api/2/user?username={user_key}&expand=groups"
+    
+    response = requests.get(url, auth=get_auth(), headers={"Accept": "application/json"})
     if response.status_code != 200:
         logging.error(f"Error fetching groups for user {user_key}: {response.status_code}")
         return False
@@ -134,7 +271,7 @@ def fetch_issue_by_key(issue_key):
     params = {
         'expand': 'changelog,comment,issuelinks'
     }
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+    response = requests.get(url, auth=get_auth(),
                             headers={"Accept": "application/json"}, params=params)
     if response.status_code != 200:
         logging.error(f"Error fetching issue {issue_key}: {response.status_code}")
@@ -143,7 +280,7 @@ def fetch_issue_by_key(issue_key):
 
 def fetch_issue_comments(issue_key):
     url = f"{config['base_url']}/rest/api/2/issue/{issue_key}?fields=comment&expand=comment"
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+    response = requests.get(url, auth=get_auth(),
                             headers={"Accept": "application/json"})
     if response.status_code != 200:
         logging.error(f"Error fetching comments for issue {issue_key}: {response.status_code}")
@@ -151,10 +288,68 @@ def fetch_issue_comments(issue_key):
     issue_data = response.json()
     return issue_data['fields']['comment']['comments'] if 'comment' in issue_data['fields'] else []
 
-def handle_user(user_key, user_cache):
+
+def handle_user(user_data, user_cache):
+    if not user_data:
+        return CUSTOM_USER
+
+    email = user_data.get('emailAddress', None)
+    user_accounts = load_user_accounts()
+
+    if JIRA_VERSION == 'cloud':
+        user_key = user_data.get('accountId', None)
+    else:
+        user_key = user_data.get('name', None)
+
     if is_user_in_exempted_groups(user_key, user_cache):
+        
+        if JIRA_VERSION == 'datacenter' and email:
+            account_id = user_accounts.get(email)
+            if not account_id:
+                account_id = fetch_account_id_by_email(email)
+                if account_id != CUSTOM_USER:
+                    user_accounts[email] = account_id
+                    save_user_accounts(user_accounts)
+                else:
+                    logging.info(f"Account ID not found for {email}, using CUSTOM_USER.")
+                    return CUSTOM_USER
+            return account_id
+
         return user_key
-    return CUSTOM_COMMENT_AUTHOR
+
+    logging.info(f"User {user_key} is not in exempted groups, returning CUSTOM_USER.")
+    return CUSTOM_USER
+
+def process_custom_fields(issue, custom_fields, mapped_issue, user_cache):
+    for field_id, field_value in issue['fields'].items():
+        if field_id.startswith("customfield_") and field_id in custom_fields and field_value:
+            custom_field_info = custom_fields[field_id]
+            
+            if isinstance(field_value, dict) and 'value' in field_value:
+                value = field_value['value']
+            elif isinstance(field_value, list):
+                value = [item['value'] if isinstance(item, dict) and 'value' in item else item for item in field_value]
+            else:
+                value = field_value
+
+            if custom_field_info['type'] == 'com.atlassian.jira.plugin.system.customfieldtypes:datetime':
+                value = format_jira_datetime(value)
+
+            if custom_field_info['type'] in ['com.atlassian.jira.plugin.system.customfieldtypes:userpicker', 
+                                             'com.atlassian.jira.plugin.system.customfieldtypes:multiuserpicker']:
+                if custom_field_info['type'] == 'com.atlassian.jira.plugin.system.customfieldtypes:userpicker':
+                    if isinstance(value, dict):
+                        value = handle_user(value, user_cache)  
+                    else:
+                        value = handle_user({'emailAddress': value}, user_cache)  
+                elif custom_field_info['type'] == 'com.atlassian.jira.plugin.system.customfieldtypes:multiuserpicker':
+                    value = [handle_user(user, user_cache) for user in field_value if isinstance(user, dict)]
+
+            mapped_issue['customFieldValues'].append({
+                "fieldName": custom_field_info['name'],
+                "fieldType": custom_field_info.get('type', 'unknown'),
+                "value": value
+            })
 
 def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_links):
     issue_key = issue['key']
@@ -176,7 +371,7 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
         "priority": issue['fields']['priority']['name'] if issue['fields'].get('priority') else None,
         "description": issue['fields'].get('description', ''),
         "status": issue['fields']['status']['name'],
-        "reporter": issue['fields']['reporter']['displayName'] if issue['fields'].get('reporter') else CUSTOM_COMMENT_AUTHOR,
+        "reporter": handle_user(issue['fields'].get('reporter'), user_cache),
         "labels": issue['fields'].get('labels', []),
         "issueType": issue['fields']['issuetype']['name'],
         "resolution": issue['fields']['resolution']['name'] if issue['fields'].get('resolution') else None,
@@ -186,7 +381,7 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
         "duedate": issue['fields'].get('duedate'),
         "affectedVersions": [v['name'] for v in issue['fields'].get('versions', [])],
         "summary": issue['fields']['summary'],
-        "assignee": issue['fields']['assignee']['displayName'] if issue['fields'].get('assignee') else CUSTOM_COMMENT_AUTHOR,
+        "assignee": handle_user(issue['fields'].get('assignee'), user_cache),
         "fixedVersions": [v['name'] for v in issue['fields'].get('fixVersions', [])],
         "components": [c['name'] for c in issue['fields'].get('components', [])],
         "customFieldValues": [],
@@ -224,21 +419,13 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
                     "destinationId": str(destination_id)
                 })
 
-    for field_id, field_value in issue['fields'].items():
-        if field_id.startswith("customfield_") and field_id in custom_fields and field_value:
-            custom_field_info = custom_fields[field_id]
-            value = field_value['value'] if isinstance(field_value, dict) and 'value' in field_value else field_value
-            mapped_issue['customFieldValues'].append({
-                "fieldName": custom_field_info['name'],
-                "fieldType": custom_field_info.get('type', 'unknown'),
-                "value": value
-            })
+    process_custom_fields(issue, custom_fields, mapped_issue, user_cache)
 
     if 'attachment' in issue['fields'] and issue['fields']['attachment']:
         mapped_issue["attachments"] = [
             {
                 "name": a['filename'],
-                "attacher": handle_user(a['author']['accountId'], user_cache),
+                "attacher": handle_user(a['author'], user_cache),
                 "created": a['created'],
                 "uri": a['content'],
                 "description": a.get('description', '')
@@ -250,7 +437,7 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
         mapped_issue["comments"] = [
             {
                 "body": c['body'],
-                "author": handle_user(c['author']['accountId'], user_cache),
+                "author": handle_user(c['author'], user_cache),
                 "created": c['created']
             } for c in comments
         ]
@@ -258,7 +445,7 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
     if 'changelog' in issue and issue['changelog'].get('histories'):
         mapped_issue["history"] = [
             {
-                "author": handle_user(h['author']['accountId'], user_cache) if h.get('author') else CUSTOM_COMMENT_AUTHOR,
+                "author": handle_user(h['author'], user_cache) if h.get('author') else CUSTOM_USER,
                 "created": h['created'],
                 "items": [
                     {
@@ -280,7 +467,7 @@ def map_issue_details(issue, custom_fields, mapped_issues, user_cache, issue_lin
 
 def fetch_custom_fields():
     url = f"{config['base_url']}/rest/api/2/field"
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+    response = requests.get(url, auth=get_auth(),
                             headers={"Accept": "application/json"})
     if response.status_code != 200:
         logging.error(f"Error fetching custom fields: {response.status_code}")
@@ -305,7 +492,7 @@ def fetch_issues(project_key):
         'maxResults': max_results,
         'expand': 'changelog'
     }
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+    response = requests.get(url, auth=get_auth(),
                             headers={"Accept": "application/json"}, params=params)
     
     if response.status_code != 200:
@@ -320,7 +507,7 @@ def fetch_issues(project_key):
 
     def fetch_issue_batch(start_at):
         params['startAt'] = start_at
-        response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+        response = requests.get(url, auth=get_auth(),
                                 headers={"Accept": "application/json"}, params=params)
         if response.status_code != 200:
             logging.error(f"Error fetching issues at startAt {start_at}: {response.status_code}")
@@ -357,7 +544,7 @@ def map_issues_in_parallel(issues, custom_fields, user_cache):
 
 def fetch_project_details(project_key):
     url = f"{config['base_url']}/rest/api/2/project/{project_key}"
-    response = requests.get(url, auth=HTTPBasicAuth(config['email'], config['token']),
+    response = requests.get(url, auth=get_auth(),
                             headers={"Accept": "application/json"})
     if response.status_code != 200:
         logging.error(f"Error fetching project details for {project_key}: {response.status_code}")
